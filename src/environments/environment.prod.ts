57fa0dd56ead9a21a7453bd8e9d23f1329b9b686
Quentin Lassalle <qlassalle@gmail.com>
2022-05-25 09:19:43 +0200

    Update API port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://192.168.1.21:30202/api',
-  allowedDomain: '192.168.1.21:30202'
+  serverUrl: 'http://192.168.1.21:31112/api',
+  allowedDomain: '192.168.1.21:31112'
 };
